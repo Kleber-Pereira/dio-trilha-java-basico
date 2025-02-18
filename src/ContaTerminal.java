d376b668e5561f20b2b8c0e2d23979be2a5c99a3
Kleber Gonçalves Pereira <kleber_master@hotmail.com>
2025-02-17 22:44:14 -0300

    finalizado projeto proposto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,24 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o número da Agência!");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite o número da Conta!");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite seu nome!");
+        String nomeCliente = scanner.next();
+        System.out.println("Por favor, digite seu saldo!");
+        double saldo = scanner.nextDouble();
+        System.out.println("Olá "+  nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+
+                agencia+", conta "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque.");
+
 
 
     }
